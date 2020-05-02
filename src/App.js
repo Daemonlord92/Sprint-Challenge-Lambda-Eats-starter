18a8e9a5bbe66896ce11a909ad6a0ea8ae0d451a
Matthew Martin <zhintateo19924559@gmail.com>
2020-05-01 20:59:44 -0500

    Used img provided to make a Background, and imported Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router,
+				Link,
+				Route } from "react-router-dom"
+import { Container,
+					Col,
+					Row,
+					Card,
+					Jumbotron,
+					Button,
+					Nav, 
+					NavItem, 
+					NavLink } from "reactstrap"
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+    	<Router>
+    		<Container>
+	    		<Jumbotron>
+		      	<h1>Lambda Eats</h1>
+		      	<Nav>
+			        <NavItem>
+			          <NavLink href="javascript:void(0)">
+			          	<Link>
+			          		Home
+			          	</Link>
+			          </NavLink>
+			        </NavItem>
+			        <NavItem>
+			          <NavLink href="javascript:void(0)">
+			          	<Link>
+			          		Order Pizza
+			          	</Link>
+			          </NavLink>
+			        </NavItem>
+			      </Nav>
+		      </Jumbotron>
+		      
+	      </Container>
+      </Router>
     </>
   );
 };
